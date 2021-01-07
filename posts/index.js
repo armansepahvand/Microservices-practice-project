971557486788a4737fdab4f03f695a3e6975a801
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-07 14:02:49 -0700

    set up the structure for post service index file

--- posts/index.js
+++ posts/index.js
@@ -1,7 +1,11 @@
-const express = require ("express");
+const express = require("express");
 
-const app= express();
+const app = express();
 
-app.get("/posts", (req,res)=>{
-    
-})
+app.get("/posts", (req, res) => {});
+
+app.post("/post", (req, res) => {});
+
+app.listen(4000, () => {
+  console.log("listening on port 4000");
+});
