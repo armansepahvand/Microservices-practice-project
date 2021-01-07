af42f436c628a8a6f63d945709ec015c50ca9ab5
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-07 14:24:40 -0700

    coppied the index.js contents from posts service to the comment service and changed the memory object name and route pathes

---
+++ comments/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(comments);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+
+  const { title } = req.body;
+
+  comments[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send(comments[id]);
+});
+
+app.listen(4001, () => {
+  console.log("listening on port 4000");
+});
