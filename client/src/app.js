5baf82e9ec5a0b5de22e115f3804cab652c21bd9
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-07 15:23:07 -0700

    created app.js file and hooked up the PostCreate component inside

---
+++ client/src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PostCreate from "./PostCreate";
+
+// eslint-disable-next-line
+export default () => {
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+    </div>
+  );
+};
