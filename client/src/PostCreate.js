02cd484414c3d23c7788915eb824caeda811b70e
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-07 15:39:36 -0700

    commants added

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -5,12 +5,14 @@ export default () => {
   //state to keep track of the Title input
   const [title, setTilte] = useState("");
 
+  //function to handle sending the form input value to backend using axios
   const onSubmit = async (event) => {
     event.preventDefault();
 
     await axios.post("http://localhost:4000/posts", {
       title,
     });
+    //set title to empty string to show empty input space after submitting
     setTilte("");
   };
 
