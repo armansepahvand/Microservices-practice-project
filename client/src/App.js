1e1f796c793c35ededce49adc4f50eea43b9f9a2
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-07 19:41:25 -0700

    created a state from posts and added the fetchpost function to fet the posts from server

--- client/src/App.js
+++ client/src/App.js
@@ -2,3 +2,13 @@ import React, { useEffect, useState } from "react";
 import PostCreate from "./PostCreate";
 import PostList from "./PostList";
 import axios from "axios";
+
+// eslint-disable-next-line
+export default () => {
+  const [posts, setPosts] = useState({});
+
+  const fetchPost = async () => {
+    const res = await axios.get("http://localhost:4000/posts");
+
+    return res.data;
+  };
