329d16abdd09bf9c53b6eb8c2dd014138ae9b5d4
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-07 19:44:54 -0700

    test commit

--- client/src/app.js
+++ client/src/app.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PostCreate from "./PostCreate";
 import PostList from "./PostList";
+import axios from "axios";
 
 // eslint-disable-next-line
 export default () => {
+  const [posts, setPosts] = useState({});
+
+  const fetchPost = async () => {
+    const res = await axios.get("http://localhost:4000/posts");
+
+    return res.data;
+  };
+
+  useEffect(async () => {
+    setPosts(await fetchPost());
+  }, []);
+
+  const onSubmit = async (title) => {
+    await axios.post("http://localhost:4000/posts", { title });
+    //set title to empty string to show empty input space after submitting
+    //setTilte("");
+    setPosts(await fetchPost());
+  };
   return (
     <div className="container">
       <h1>Create Post</h1>
-      <PostCreate />
+      <PostCreate onSubmit={onSubmit} />
       <hr />
       <h1>Posts</h1>
-      <PostList />
+      <PostList posts={posts} />
     </div>
   );
 };
