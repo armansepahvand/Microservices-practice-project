194edfc09ec82f3fe127a1905ecac0653fcfd990
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-08 21:55:22 -0700

    added axios post request to our event-bus to send the new post to event-bus

--- posts/index.js
+++ posts/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
 //posts object to temporarily save the created posts in memory
 const posts = {};
@@ -16,7 +17,7 @@ app.get("/posts", (req, res) => {
 });
 
 //post method to create posts
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   //create a randome id for each post
   const id = randomBytes(4).toString("hex");
 
@@ -27,6 +28,14 @@ app.post("/posts", (req, res) => {
     id,
     title,
   };
+
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
 
   res.status(201).send(posts[id]);
 });
