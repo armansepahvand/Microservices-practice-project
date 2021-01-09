ca91796a0772a0b0a7dc6dc9d162edafdc691327
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-09 15:37:55 -0700

    refacored the event handling logic into eventHandler function

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,13 +10,7 @@ app.use(cors());
 //post object to save the posts and their comments
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  res.send(posts);
-});
-
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
-
+const eventHandler = (type, data) => {
   if (type === "PostCreated") {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
@@ -25,7 +20,20 @@ app.post("/events", (req, res) => {
     const post = posts[postId];
     post.comments.push({ id, content, status });
   }
-  console.log(posts);
+
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+    console.log(status);
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id == id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+};
 
   res.send({});
 });
