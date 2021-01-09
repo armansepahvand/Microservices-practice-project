8899543f163ef24faa932939e68a5b746ced0f3e
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-08 21:35:30 -0700

    created commentCreate and CommentList components and added them to PostList componenet

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 
 export default ({ posts }) => {
   const [postlist, setPostlist] = useState({});
@@ -16,6 +18,8 @@ export default ({ posts }) => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentList postId={post.id} />
+          <CommentCreate postId={post.id} />
         </div>
       </div>
     );
